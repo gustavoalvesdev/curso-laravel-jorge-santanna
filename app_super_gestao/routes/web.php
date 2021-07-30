96951ddc0332f7dbffa5aeaca22d3d5957efea41
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-07-29 23:25:26 -0300

    naming routes

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -16,28 +16,28 @@ use App\Http\Controllers\ContatoController;
 */
 
 // SITE
-Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 
-Route::get('/contato', [ContatoController::class, 'contato']);
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 
 Route::get('/login', function() {
     return 'Login';
-});
+})->name('site.login');
 
 // APP
 Route::prefix('/app')->group(function() {
     Route::get('/clientes', function() {
         return 'Clientes';
-    });
+    })->name('app.clientes');
 
     Route::get('/fornecedores', function() {
         return 'Fornecedores';
-    });
+    })->name('app.fornecedores');
 
     Route::get('/produtos', function() {
         return 'Produtos';
-    });
+    })->name('app.produtos');
 });
 
