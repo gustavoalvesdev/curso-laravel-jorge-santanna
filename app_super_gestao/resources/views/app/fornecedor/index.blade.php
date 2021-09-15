121be47a908f08a14cc0de25105fa7071bb207b8
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-09-15 02:51:03 -0300

    45. Blade - @if/@else

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,15 +1,21 @@
 <h3>Fornecedor</h3>
 
-{{ 'Texto de teste' }}
-<?= 'Texto de teste' ?>
-
-{{-- Fica o comentário que será descartado pelo interpretador do Blade --}}
-
 @php
-    // Para comentários de uma linha
     /*
-        Para comentários de múltiplas linhas
-    */
+    if () {
 
-    echo 'Texto de teste';
+    } elseif () {
+
+    } else {
+
+    }
+    */
 @endphp
+
+@if (count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
