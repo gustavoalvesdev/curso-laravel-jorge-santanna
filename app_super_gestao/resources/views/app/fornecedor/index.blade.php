97d6b9adfa3ff2e4d246ec50a0916374a4afcafc
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-09-21 20:20:17 -0300

    46. Blade - @unless

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -2,20 +2,21 @@
 
 @php
     /*
-    if () {
-
-    } elseif () {
-
-    } else {
-
     }
     */
 @endphp
 
-@if (count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem vários fornecedores cadastrados</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados</h3>
+
+
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Status: {{ $fornecedores[0]['status'] }}
+<br>
+@if(! ($fornecedores[0]['status'] == 'S') )
+    Fornecedor inativo
 @endif
+<br>
+@unless($fornecedores[0]['status'] == 'S') <!-- se o retorno da condição for false -->
+    Fornecedor inativo
+@endunless
+<br>
