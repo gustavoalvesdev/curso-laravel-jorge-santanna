ffbec20b2372bdc94f78340df57f3809d350b6da
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-09-21 20:28:54 -0300

    47. Blade - @isset

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -6,17 +6,12 @@
     */
 @endphp
 
-
-
-Fornecedor: {{ $fornecedores[0]['nome'] }}
-<br>
-Status: {{ $fornecedores[0]['status'] }}
-<br>
-@if(! ($fornecedores[0]['status'] == 'S') )
-    Fornecedor inativo
-@endif
-<br>
-@unless($fornecedores[0]['status'] == 'S') <!-- se o retorno da condição for false -->
-    Fornecedor inativo
-@endunless
-<br>
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status'] }}
+    <br>
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{ $fornecedores[0]['cnpj'] }}
+    @endisset
+@endisset
